da413102aa2fbae8cd2e509b05ac91978ca52dca
Jyothi Sumer <mjyothisumer@gmail.com>
2022-05-17 12:47:53 -0400

    finished game development part

--- Grid.js
+++ Grid.js
@@ -50,6 +50,7 @@ class Cell {
     #x
     #y
     #tile
+    #mergeTile
 
     constructor(cellElement, x, y) {
         this.#cellElement = cellElement;
@@ -69,12 +70,34 @@ class Cell {
         return this.#tile
     }
 
+    get mergeTile() {
+        return this.#mergeTile
+    }
+
     set tile(value) {
         this.#tile = value
         if (value == null) return
         this.#tile.x = this.#x
         this.#tile.y = this.#y
 
+    }
+
+    set mergeTile(value) {
+        this.#mergeTile = value
+        if (value == null) return
+        this.#mergeTile.x = this.#x
+        this.#mergeTile.y = this.#y
+    }
+
+    canAccept(tile) {
+        return (this.tile == null || (this.mergeTile == null && this.tile.value === tile.value))
+    }
+
+    mergeTiles() {
+        if (this.tile == null || this.mergeTile == null) return
+        this.tile.value = this.tile.value + this.mergeTile.value
+        this.mergeTile.remove()
+        this.mergeTile == null
     }
 }
 
--- Tile.js
+++ Tile.js
@@ -1,32 +1,52 @@
 export default class Tile {
-    #titleElement
+    #tileElement
     #x
     #y
     #value
 
     constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
-        this.#titleElement = document.createElement("div")
-        this.#titleElement.classList.add("tile")
-        tileContainer.append(this.#titleElement)
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
         this.value = value
+    }
+
+    get value() {
+        return this.#value
     }
 
     set value(v) {
         this.#value = v
-        this.#titleElement.textContent = v;
+        this.#tileElement.textContent = v;
         const power = Math.log2(v)
         const backgroundLightness = 100 - power * 9;
-        this.#titleElement.style.setProperty("--background-lightness", `${backgroundLightness}%`)
-        this.#titleElement.style.setProperty("--text-lightness", `${backgroundLightness > 50 ? 10 : 90}%`)
+        this.#tileElement.style.setProperty("--background-lightness", `${backgroundLightness}%`)
+        this.#tileElement.style.setProperty("--text-lightness", `${backgroundLightness > 50 ? 10 : 90}%`)
     }
 
     set x(value) {
         this.#x = value
-        this.#titleElement.style.setProperty("--x", value)
+        this.#tileElement.style.setProperty("--x", value)
     }
 
     set y(value) {
         this.#y = value
-        this.#titleElement.style.setProperty("--y", value)
+        this.#tileElement.style.setProperty("--y", value)
+    }
+
+    remove() {
+        this.#tileElement.remove()
+    }
+
+    waitForTransition(animation = false) {
+        return new Promise(resolve => {
+            this.#tileElement.addEventListener(
+                animation ? "animationend" : "transitionend",
+                resolve,
+                {
+                    once: true,
+                }
+            )
+        })
     }
 }
